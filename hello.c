2c5e6495d364f213adf5332c69b5b6227185c667
thompsonseanr <thompson.sean.r@gmail.com>
2024-04-02 20:59:24 -0700

    again

--- hello.c
+++ hello.c
@@ -2,6 +2,6 @@
 #include <stdlib.h>
 
 int main(int argc, char *argv[]) {
-  puts("HHello, world!!!! Testing the automated repo build. Again. And again.");
+  puts("HHHello, world!!!! Testing the automated repo build. Again. And again.");
   return EXIT_SUCCESS;
 }
